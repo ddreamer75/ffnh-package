ee3053c9d14879ae2513a2340e32b6d7cf4381ef
Michael H. <35325905+ddreamer75@users.noreply.github.com>
2026-01-28 10:48:00 +0100

    Delete ffbs-mesh-vpn-parker directory

--- ffbs-mesh-vpn-parker/check_site.lua
+++
@@ -1,4 +0,0 @@
-need_string({ "parker", "config_server" })
-need_string({ "parker", "config_pubkey" })
-need_string_array({'ntp_servers'})
-need_number({ "mesh_vpn", "parker", "mtu" })
--- ffbs-mesh-vpn-parker/files/usr/sbin/drop_caches.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-LOGGER="logger -s -t drop_caches.sh"
-$LOGGER Dropping caches.
-
-echo 3 > /proc/sys/vm/drop_caches
